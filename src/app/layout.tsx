fe1b3793cad02f09ef1cea3b3f1857ff16916dec
Thanos Kyriakakis <thanos.kyriakakis@persgroep.net>
2024-09-21 10:46:15 +0200

    add new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Darker_Grotesque } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const darkerGrotesqueFont = Darker_Grotesque({ subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,8 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${darkerGrotesqueFont.className} antialiased`}
       >
+        <div>Thanos is here!</div>
         {children}
       </body>
     </html>
