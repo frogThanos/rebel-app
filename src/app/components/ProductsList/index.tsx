18678ab7945b523872df7641b82f2630846be7e7
Thanos Kyriakakis <thanos.kyriakakis@persgroep.net>
2024-09-22 10:44:48 +0200

    render list of products

--- src/app/components/ProductsList/index.tsx
+++ src/app/components/ProductsList/index.tsx
@@ -1,9 +1,29 @@
+"use client"
 import React from "react";
+import Image from 'next/image';
+import { Title, HeadingLevel} from '../Title';
+import useFetchProducts from '../../hooks/useFetchProducts';
 
 const ProductsList = () => {
-  return(
-    <div>ProductsList</div>
-  )
+  const { products, loading, error } = useFetchProducts();
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <Title text={product.name} level={HeadingLevel.H2} />
+            <p>Brand: {product.brand}</p>
+            <p>Category: {product.category}</p>
+            <Image src={product.image} alt={product.name} width={100} height={100} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ProductsList;
